a50d34ce7da6c0cc54dd06d53612ac6d5672695d
bryan1993-HA <thoury.bryan1993@gmail.com>
2025-08-12 01:37:30 +0200

    Update run.sh

--- domovra/run.sh
+++ domovra/run.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/with-contenv bashio
+set -e
 
+WARNING_DAYS=$(bashio::config 'retention_days_warning')
+CRITICAL_DAYS=$(bashio::config 'retention_days_critical')
+export WARNING_DAYS CRITICAL_DAYS
+
+# Dossier de données (persiste dans les backups HA)
+export DB_PATH="/data/domovra.sqlite3"
+
+# Lancer l'app FastAPI (Ingress)
+exec uvicorn main:app --host 0.0.0.0 --port 8099 --app-dir /opt/app
